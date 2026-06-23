36fb86fa52c5a96c2bc92011c0c81ec06a3f2554
ruanlenzzi <ruan.lenzzi.sm@gmail.com>
2026-06-22 22:53:16 -0300

    Update Produto.java

---
+++ quiosque/projeto/src/model/Produto.java
@@ -0,0 +1,25 @@
+package model;
+
+public abstract class Produto implements IProduto {
+    protected int id;
+    protected String nome;
+    protected double preco;
+
+    public Produto(int id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    @Override
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public double getPreco() {
+        return preco;
+    }
+
+    public abstract String exibirDetalhes();
+}
